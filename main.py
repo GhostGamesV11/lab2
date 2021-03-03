f1fcc5a97e8b519e384c30fe18040c4f5ef9b08c
Ghost <ghostgamesv11@gmail.com>
2021-03-03 11:12:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+#zad1
+#lista = ["piłka nożna","siatkówka","pływanie"]
+
+#lista.reverse()
+#lista.insert(4,"tenis")
+#lista.insert(5,"skok w dal")
+#print(lista)
+
+#zad4
+# z=input("Podaj dowolne zdanie ")
+# suma=0
+# for char in z:
+#     if char=="a":
+#         suma+=1
+# print("Ilość litery A w zdaniu:",suma)
+
+#zad5
+#print("Podaj 3 liczby całkowite: ")
+#l1=float(input("Podaj 1 liczbe:"))
+#l2=float(input("Podaj 2 liczbe:"))
+#l3=float(input("Podaj 3 liczbe:"))
+#wynik=l1**l2+l3
+#print("Wynik wynosi: ",wynik)
+
+#zad6
+# print("podaj 3 liczby całkowite")
+# a=float(input("Podaj 1 liczbe:"))
+# b=float(input("Podaj 2 liczbe:"))
+# c=float(input("Podaj 3 liczbe:"))
+#
+# if a>=b:
+#     if a>c:
+#         wynik=a
+#     elif c>a:
+#         wynik=c
+# elif b>=a:
+#     if b>c:
+#         wynik=b
+#     elif c>b:
+#         wynik=c
+# print("Najwieksza liczba to: ",wynik)
+
+#zad7
+# lista2=[2.5,3,3.5,4]
+# for x in lista2:
+#     x=x**2
+#     print(x)
+
+# #zad8
+# lista3=[1,2,3,4,5,6,7,8,9,10]
+# parzyste=0
+# for x in lista3:
+#     while x%2==0:
+#         parzyste+=1
+#         print(parzyste)
+
+#zad10
+# import math
+# p=float(input("Podaj liczbe:"))
+# if p>0:
+#     p=math.sqrt(p)
+#     print("Pierwiastek z tej liczby to:",p)
+# elif p<0:
+#     print("Podałeś złą liczbe!!!!")
+
+
+
